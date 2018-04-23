444d8cf08c57ff4e52f11b4a004ed96239292600
Will Son <willson@robotis.com>
2018-04-23 09:25:15 +0900

    conflict resolved

--- install.sh
+++ install.sh
@@ -42,11 +42,7 @@ DEPENDENCY_OUTPUT=$(arduino --install-boards OpenCM904:OpenCM904 2>&1)
 if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
 
 # Update OpenCR package manually
-<<<<<<< HEAD
-git clone --recursive https://github.com/ROBOTIS-GIT/OpenCM9.04.git --branch develop --single-branch 
-=======
 git clone --recursive https://github.com/ROBOTIS-GIT/OpenCM9.04.git --branch develop --single-branch
->>>>>>> refs/remotes/origin/master
 rm -rf $HOME/.arduino15/packages/OpenCM904/hardware
 mkdir $HOME/Arduino/hardware
 mkdir $HOME/Arduino/hardware/OpenCM904
